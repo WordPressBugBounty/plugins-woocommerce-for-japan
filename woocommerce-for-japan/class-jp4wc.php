c135edc66332017c8d1a9703ebb4d2602e322a50
Sean Murphy <sean@iamseanmurphy.com>
2025-09-08 01:48:33 +0000

    2.7.5

--- woocommerce-for-japan/class-jp4wc.php
+++ woocommerce-for-japan/class-jp4wc.php
@@ -3,7 +3,7 @@
  * Main class file for Japanized for WooCommerce
  *
  * @package Japanized for WooCommerce
- * @since 2.7.3
+ * @since 2.7.4
  */
 
 if ( ! class_exists( 'JP4WC' ) ) :
@@ -21,7 +21,7 @@ if ( ! class_exists( 'JP4WC' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '2.7.3';
+		public $version = '2.7.4';
 
 		/**
 		 * Japanized for WooCommerce Framework version.
@@ -84,7 +84,6 @@ if ( ! class_exists( 'JP4WC' ) ) :
 			if ( ! get_transient( 'jp4wc_first_installing' ) ) {
 				// First time installing.
 				set_transient( 'jp4wc_first_installing', 'yes', 180 * DAY_IN_SECONDS );
-				update_option( 'wc4jp-tracking', true );
 			}
 		}
 
